76f38957439e5b4935e1e2f14aadea5562b74c17
Dylan Ross Ludwick <dylan@devhominid.com>
2017-11-09 12:31:28 -0700

    add redis and config

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import 'babel-polyfill';
+import redis from 'redis';
+import axios from 'axios';
 import OpenSecretsCall from '../opensecrets-api';
-
 
 // Dotenv config
 dotenv.config();
+
+// Create new redis client and connect to redis instance
+const redisClient = redis.createClient();
+redisClient.on('error', (err) => {
+  console.log(`Error: ${err}`);
+});
 
 // Init app
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+// app.get('/', (req, res, next) => {
+//   const getLegislators = new OpenSecretsCall('getLegislators', { id: 'NJ' });
+//   getLegislators.fetchData()
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// });
 
 // Error logging middleware
 app.use((err, req, res, next) => {
